bc960a5830d72cfc8fb4ae1a1b0a23c26ef393de
danlwright <danbran04@gmail.com>
2017-06-07 12:24:22 -0700

    Add message if no data

--- C5A1/js/app.js
+++ C5A1/js/app.js
@@ -12,7 +12,7 @@
         $scope.sayMessage = function() {
             var menuItemsString = $scope.menuItems.split(',');
             var menuItemsLength = menuItemsString.length;
-            if ($scope.menuItems == "") {
+            if ($scope.menuItems.trim() == "") {
                 $scope.response = "Please enter data first";
             } else if (menuItemsLength < 4) {
                 $scope.response = "Enjoy!";
