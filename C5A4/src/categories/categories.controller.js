b6b70d5ecf26e5961ca954e7bc5bd527b119190d
Dan Wright <danbran04@gmail.com>
2017-07-19 12:44:29 -0700

    Add files via upload

---
+++ C5A4/src/categories/categories.controller.js
@@ -0,0 +1,14 @@
+(function() {
+    'use strict';
+
+    angular.module('MenuApp')
+        .controller('CategoriesController', CategoriesController);
+
+    CategoriesController.$inject = ['categoriesdata'];
+
+    function CategoriesController(categoriesdata) {
+        var ctrl = this;
+        ctrl.categories = categoriesdata;
+    }
+
+})();
